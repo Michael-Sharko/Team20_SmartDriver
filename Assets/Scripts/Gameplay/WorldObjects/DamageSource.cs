c049851343808c62626e98e3a7fa06b217bbc416
abamenshik <@abamenshik>
2025-03-10 15:52:39 +0300

    воспроизводится звук при столкновении машинки с тем, что дамажит при столкновении (александр не до конца доделал)

--- Assets/Scripts/Gameplay/WorldObjects/DamageSource.cs
+++ Assets/Scripts/Gameplay/WorldObjects/DamageSource.cs
@@ -1,10 +1,33 @@
+using System;
 using Shark.Gameplay.WorldObjects;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
+[RequireComponent(typeof(AudioSource))]
 public class DamageSource : MonoBehaviour, IDamageSource
 {
+    [Serializable]
+    public class PlaySoundOnDamage
+    {
+        [FormerlySerializedAs("dealDamageSound")]
+        [SerializeField] private AudioClip sound;
+        [SerializeField, Min(0)] private float volume = 1;
+
+        private AudioSource source;
+
+        public void Init(AudioSource source, ref Action onDamage)
+        {
+            this.source = source;
+            onDamage += Play;
+        }
+        private void Play()
+        {
+            if (!sound || !source)
+                return;
+
+            source.PlayOneShot(sound, volume);
+        }
+    }
     [SerializeField]
     private float _damage;
 
@@ -14,12 +37,17 @@ public class DamageSource : MonoBehaviour, IDamageSource
     [SerializeField]
     private float _multiplierDamageThresholdCollisionForce = 10;
 
+    [SerializeField]
+    private PlaySoundOnDamage soundsOnDamage;
+
 #if UNITY_EDITOR
     [field: SerializeField, Header("Debug-Constant")]
     private float LastCollisionForce { get; set; }
 #endif
 
     private float _lastCollisionForce;
+
+    public event Action OnDealDamage;
 
 #if UNITY_EDITOR
     private void Update()
@@ -28,8 +56,15 @@ public class DamageSource : MonoBehaviour, IDamageSource
     }
 #endif
 
+    private void Awake()
+    {
+        soundsOnDamage.Init(GetComponent<AudioSource>(), ref OnDealDamage);
+    }
+
     public void DealDamage(IBreakable breakable)
     {
+        OnDealDamage?.Invoke();
+
         breakable.TakeDamage(_damage);
     }
 
@@ -37,7 +72,7 @@ public class DamageSource : MonoBehaviour, IDamageSource
     {
         if (collision.transform.TryGetComponent(out IBreakable breakable) &&
             collision.transform.TryGetComponent(out Rigidbody rigidbody))
-        {            
+        {
             _lastCollisionForce = collision.impulse.magnitude / rigidbody.mass * _multiplierDamageThresholdCollisionForce;
             if (_lastCollisionForce >= _damageThresholdCollisionForce)
             {
@@ -46,3 +81,4 @@ public class DamageSource : MonoBehaviour, IDamageSource
         }
     }
 }
+
