b7d7b1278d00bf5baaf7618f5c6b512ea1625174
abamenshik <@abamenshik>
2025-03-27 20:18:52 +0300

    переработал воспроизведение звуков

--- Assets/Scripts/Gameplay/Settings/BaseGetSound.cs
+++ Assets/Scripts/Gameplay/Settings/BaseGetSound.cs
@@ -3,4 +3,19 @@
 public abstract class BaseGetSound : ScriptableObject
 {
     public abstract AudioClip GetClip();
+
+    protected AudioClip LoadClip(string path)
+    {
+        var clip = Resources.Load<AudioClip>(path);
+        //clip.completed += Operation_completed;
+        return clip;
+    }
+
+    //private void Operation_completed(AsyncOperation obj)
+    //{
+    //    if (obj.isDone)
+    //    {
+
+    //    }
+    //}
 }
