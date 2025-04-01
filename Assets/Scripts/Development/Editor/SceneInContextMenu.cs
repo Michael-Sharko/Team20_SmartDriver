0681de416f1af4b56106109813513c7aa5e0260f
abamenshik <@abamenshik>
2025-04-01 19:14:08 +0300

    переключение между сценами в редакторе в контекстном меню

---
+++ Assets/Scripts/Development/Editor/SceneInContextMenu.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class SceneInContextMenu
+{
+    [MenuItem("Scene/Refresh", false, 0)]
+    public static void Refresh()
+    {
+        var path = Application.dataPath + "/Scripts/Development/Editor/SceneInContextMenu_Generated.cs";
+
+        StringBuilder stringBuilder = new();
+
+        stringBuilder.Append($@"
+// =================================================================
+//
+//               THIS CODE IS GENERATED AUTOMATICALLY
+//                         DO NOT EDIT
+//
+// =================================================================
+
+using UnityEditor.SceneManagement;
+using UnityEditor;
+
+public class {nameof(SceneInContextMenu)}_Generated {{
+");
+        var scenes = GetListScenesPaths();
+
+        for (int i = 0; i < scenes.Count; i++)
+        {
+            var name = PathToName(scenes[i]);
+            stringBuilder.Append($@"
+    [MenuItem(""Scene/{name}"")]
+    public static void Scene_{i + 1}() 
+    {{
+        EditorSceneManager.OpenScene(""{scenes[i]}"", OpenSceneMode.Single);
+    }}
+");
+        }
+        stringBuilder.Append("}");
+
+        File.WriteAllText(path, stringBuilder.ToString());
+
+        AssetDatabase.Refresh();
+    }
+    private static List<string> GetListScenesPaths()
+    {
+        var tmp = new List<string>();
+
+        for (var i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
+        {
+            var scenePath = SceneUtility.GetScenePathByBuildIndex(i);
+            tmp.Add(scenePath);
+        }
+        return tmp;
+    }
+    private static string PathToName(string path)
+    {
+        var lastSlash = path.LastIndexOf("/", StringComparison.Ordinal);
+        var name = path.Substring(lastSlash + 1, path.LastIndexOf(".", StringComparison.Ordinal) - lastSlash - 1);
+        return name;
+    }
+}
