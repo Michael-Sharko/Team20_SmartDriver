f1bf39eb956b3479fa02336ab5ca61a2fb2dafb6
abamenshik <@abamenshik>
2025-05-13 08:26:51 +0300

    Корректировка механики перекати-поля

--- Assets/Scripts/Gameplay/WorldObjects/Tumbleweed.cs
+++ Assets/Scripts/Gameplay/WorldObjects/Tumbleweed.cs
@@ -13,6 +13,7 @@ public class Tumbleweed : MonoBehaviour, IActivatable
     private bool _isTriggered;
     private Rigidbody _rigidbody;
     private ConstantForce _constantForce;
+    private Vector3 currentDirection;
 
     public event Action OnActivate;
 
@@ -38,28 +39,48 @@ public class Tumbleweed : MonoBehaviour, IActivatable
 
         soundOnActivate.Init(ref OnActivate, GetComponent<AudioSource>());
 
-        var xDirection = Mathf.Cos(directionAngle * Mathf.Deg2Rad);
-        var yDirection = Mathf.Sin(directionAngle * Mathf.Deg2Rad);
-        var direction = new Vector3(xDirection, 0, yDirection);
-        _constantForce.force = direction * speed;
+        SetDirectionAtAngle();
 
         _rigidbody.isKinematic = true;
     }
+    private void SetDirectionAtAngle()
+    {
+        var xDirection = Mathf.Cos(directionAngle * Mathf.Deg2Rad);
+        var yDirection = Mathf.Sin(directionAngle * Mathf.Deg2Rad);
 
+        currentDirection = new Vector3(xDirection, 0, yDirection);
+
+        _constantForce.force = currentDirection * speed;
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider is TerrainCollider)
             return;
 
-        _rigidbody.isKinematic = true;
+        ChangeDirection(collision);
+    }
+    private void ChangeDirection(Collision collision)
+    {
+        var contacts = collision.contacts;
+
+        currentDirection = transform.position - contacts[0].point;
+        currentDirection.y = 0;
+
+        _constantForce.force = currentDirection.normalized * speed;
     }
 
 
 #if UNITY_EDITOR
     private void OnDrawGizmos()
     {
-        var direction = new Vector3(Mathf.Cos(directionAngle * Mathf.Deg2Rad), 0, Mathf.Sin(directionAngle * Mathf.Deg2Rad));
-        var rotation = Quaternion.LookRotation(direction);
+        var direction = new Vector3(
+            Mathf.Cos(directionAngle * Mathf.Deg2Rad),
+            0,
+            Mathf.Sin(directionAngle * Mathf.Deg2Rad));
+
+        var isPlayMode = UnityEditor.EditorApplication.isPlaying;
+
+        var rotation = Quaternion.LookRotation(isPlayMode ? currentDirection : direction);
         UnityEditor.Handles.ArrowHandleCap(0, transform.position, rotation, 10, EventType.Repaint);
     }
 #endif
