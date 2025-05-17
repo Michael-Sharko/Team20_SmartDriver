c69bb20c65db92b3c23f109a25e65b0f0c0644cf
abamenshik <@abamenshik>
2025-05-17 12:16:43 +0300

    скрипт, генерирующий пути на ассеты в папке ресурсов

---
+++ Assets/Scripts/Development/ResourcesPaths_Generated.cs
@@ -0,0 +1,174 @@
+
+// =================================================================
+//
+//               THIS CODE IS GENERATED AUTOMATICALLY
+//                         DO NOT EDIT
+//
+// =================================================================
+
+public static class PathResources {
+public static class Audio {
+/// <summary>
+/// asset
+/// </summary>
+public static readonly string AudioSetting = "Audio/AudioSetting";
+/// <summary>
+/// mixer
+/// </summary>
+public static readonly string mixer = "Audio/mixer";
+}
+public static class Prefabs {
+/// <summary>
+/// prefab
+/// </summary>
+public static readonly string Sound = "Prefabs/Sound";
+/// <summary>
+/// prefab
+/// </summary>
+public static readonly string SoundWhile = "Prefabs/SoundWhile";
+}
+public static class Sounds {
+public static class Car {
+/// <summary>
+/// wav
+/// </summary>
+public static readonly string Low_Fuel = "Sounds/Car/Low Fuel";
+}
+public static class Crash {
+/// <summary>
+/// wav
+/// </summary>
+public static readonly string Crash1 = "Sounds/Crash/Crash1";
+/// <summary>
+/// wav
+/// </summary>
+public static readonly string Crash2 = "Sounds/Crash/Crash2";
+/// <summary>
+/// wav
+/// </summary>
+public static readonly string Crash3 = "Sounds/Crash/Crash3";
+/// <summary>
+/// wav
+/// </summary>
+public static readonly string Crash4 = "Sounds/Crash/Crash4";
+/// <summary>
+/// mp3
+/// </summary>
+public static readonly string Crash5 = "Sounds/Crash/Crash5";
+}
+public static class PickUp {
+/// <summary>
+/// wav
+/// </summary>
+public static readonly string Off_Road_T5 = "Sounds/PickUp/Off-Road T5";
+}
+public static class Tumbleweed {
+/// <summary>
+/// wav
+/// </summary>
+public static readonly string Wind_1 = "Sounds/Tumbleweed/Wind_1";
+/// <summary>
+/// wav
+/// </summary>
+public static readonly string Wind_2 = "Sounds/Tumbleweed/Wind_2";
+}
+public static class Varan {
+/// <summary>
+/// wav
+/// </summary>
+public static readonly string Varan_Attack = "Sounds/Varan/Varan Attack";
+/// <summary>
+/// wav
+/// </summary>
+public static readonly string Varan_Chase = "Sounds/Varan/Varan Chase";
+/// <summary>
+/// wav
+/// </summary>
+public static readonly string Varan_Detection = "Sounds/Varan/Varan Detection";
+}
+/// <summary>
+/// wav
+/// </summary>
+public static readonly string Falling_stones_sound_test_ = "Sounds/Falling stones sound(test)";
+/// <summary>
+/// mp3
+/// </summary>
+public static readonly string Rolling_stone_sound_test_ = "Sounds/Rolling stone sound(test)";
+}
+public static class UI {
+public static class dashboard {
+/// <summary>
+/// png
+/// </summary>
+public static readonly string arrow_fuel = "UI/dashboard/arrow_fuel";
+/// <summary>
+/// png
+/// </summary>
+public static readonly string arrow_speed = "UI/dashboard/arrow_speed";
+/// <summary>
+/// png
+/// </summary>
+public static readonly string bar = "UI/dashboard/bar";
+/// <summary>
+/// png
+/// </summary>
+public static readonly string ind_fix = "UI/dashboard/ind_fix";
+}
+public static class FuelGauge {
+/// <summary>
+/// png
+/// </summary>
+public static readonly string fuel2 = "UI/FuelGauge/fuel2";
+/// <summary>
+/// png
+/// </summary>
+public static readonly string fuel7 = "UI/FuelGauge/fuel7";
+}
+public static class Strength {
+/// <summary>
+/// png
+/// </summary>
+public static readonly string Strength_0 = "UI/Strength/Strength 0";
+/// <summary>
+/// png
+/// </summary>
+public static readonly string Strength_1 = "UI/Strength/Strength 1";
+/// <summary>
+/// png
+/// </summary>
+public static readonly string Strength_2 = "UI/Strength/Strength 2";
+/// <summary>
+/// png
+/// </summary>
+public static readonly string Strength_3 = "UI/Strength/Strength 3";
+/// <summary>
+/// png
+/// </summary>
+public static readonly string Strength_4 = "UI/Strength/Strength 4";
+/// <summary>
+/// png
+/// </summary>
+public static readonly string Strength_5 = "UI/Strength/Strength 5";
+}
+}
+public static class AAA {
+/// <summary>
+/// png
+/// </summary>
+public static readonly string Capsule = "AAA/Capsule";
+}
+public static class AAA_1 {
+/// <summary>
+/// png
+/// </summary>
+public static readonly string Capsule = "AAA 1/Capsule";
+}
+/// <summary>
+/// prefab
+/// </summary>
+public static readonly string New_Prefab_1 = "New Prefab 1";
+/// <summary>
+/// prefab
+/// </summary>
+public static readonly string New_Prefab = "New Prefab";
+}
