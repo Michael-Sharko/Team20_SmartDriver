184cce096a2d3e74bdc72b5f63314ead3f9ee98b
abamenshik <danchikafonin156@mail.ru>
2025-06-05 12:48:45 +0300

    камера не так сильно зумит в деревьях

---
+++ Assets/Scripts/Development/CameraGizmos.cs
@@ -0,0 +1,19 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+namespace Scripts.Development
+{
+#if UNITY_EDITOR
+    public class CameraGizmos : MonoBehaviour
+    {
+        private void OnDrawGizmos()
+        {
+            var collider = GetComponent<CinemachineCollider>();
+            if (collider)
+            {
+                Gizmos.DrawSphere(transform.position, collider.m_CameraRadius);
+            }
+        }
+    } 
+#endif
+}
