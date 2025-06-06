dfa51992afd6dcd3af0ab5994ec5c6f7e3f7bc44
abamenshik <danchikafonin156@mail.ru>
2025-06-06 13:31:23 +0300

    знаки можно сбить

---
+++ Assets/Scripts/Development/Debug/SignFallMessage.cs
@@ -0,0 +1,24 @@
+﻿using Scripts.Extension;
+using Scripts.Gameplay.Components;
+using UnityEngine;
+
+namespace Scripts.Development.Debug
+{
+    public class SignFallMessage : MonoBehaviour
+    {
+        private SignBreakEvent @event;
+        private void Awake()
+        {
+            @event = GetComponent<SignBreakEvent>();
+            @event.onJointBreak.AddListener(Log);
+        }
+        private void OnDestroy()
+        {
+            @event.onJointBreak.RemoveListener(Log);
+        }
+        private void Log()
+        {
+            UnityEngine.Debug.Log($"Знак {gameObject.name.Color(TextColor.red)} упал!", gameObject);
+        }
+    }
+}
